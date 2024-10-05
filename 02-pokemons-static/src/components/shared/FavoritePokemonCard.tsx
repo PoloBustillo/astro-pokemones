86e64599edffa5808df8534f88ddb7926179c98c
PoloBustillo <39663418+PoloBustillo@users.noreply.github.com>
2024-10-05 10:28:38 -0600

    favoritos end

--- 02-pokemons-static/src/components/shared/FavoritePokemonCard.tsx
+++ 02-pokemons-static/src/components/shared/FavoritePokemonCard.tsx
@@ -1,20 +1,41 @@
 import React, { useEffect, useState } from "react";
 
 const FavoritePokemonCard = ({ name, id }: { name: string; id: string }) => {
-  const [image, setimage] = useState<string>("");
-  useEffect(() => {}, []);
-
+  const removeFromLocalStorage = (id: string) => {
+    const pokemons = localStorage.getItem("favoritePokemons");
+    if (pokemons) {
+      const newPokemons = JSON.parse(pokemons).filter(
+        (pokemon: { id: string }) => pokemon.id !== id
+      );
+      localStorage.setItem("favoritePokemons", JSON.stringify(newPokemons));
+      window.location.reload();
+    }
+  };
   return (
     <div>
-      <a href={`/pokemons/${name}?`}>
-        <div className="rounded flex flex-col justify-center items-center p-3">
-          <div className="flex flex-row">
-            <div>
-              <img className="border-none w-16 h-16" src={image} alt={name} />
+      <div className="rounded flex flex-col justify-center items-center p-3">
+        <div className="flex flex-col">
+          <a href={`/pokemons/${name}?`}>
+            <div className="self-center">
+              <img
+                style={{ viewTransitionName: `${name}-image` }}
+                className="border-none w-16 h-16"
+                src={`https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/${id}.png`}
+                alt={name}
+              />
             </div>
-          </div>
+            <div className="ml-2">
+              <p className="text-sm text-center text-gray-600">{name}</p>
+            </div>
+          </a>
+          <button
+            onClick={() => removeFromLocalStorage(id)}
+            className="mt-2 bg-red-500 hover:bg-red-700 text-white font-bold py-1 px-2 rounded"
+          >
+            Eliminar
+          </button>
         </div>
-      </a>
+      </div>
     </div>
   );
 };
